5126bddfe409bb4cd512dc0957daf547f981754b
syedmohdtaha-cs <62473304+syedmohdtaha-cs@users.noreply.github.com>
2022-01-20 20:32:24 +0530

    Delete backend directory

--- backend/utils.py
+++
@@ -1,7 +0,0 @@
-from django.core.mail import send_mail
-from django.conf import settings
-
-class Util:
-    @staticmethod
-    def sent_email(data, email):
-        send_mail(data['email_subject'], data['email_body'], settings.EMAIL_HOST_USER, [email])
